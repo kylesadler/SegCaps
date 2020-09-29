b82487ed9d1ea691f6a5634cf19fb537e0a420c4
kylesadler <kylesadler5@gmail.com>
2020-09-29 12:34:43 -0500

    update

--- custom_losses.py
+++ custom_losses.py
@@ -131,6 +131,9 @@ def weighted_binary_crossentropy_loss(pos_weight):
             output = tf.clip_by_value(output, _epsilon, 1 - _epsilon)
             output = tf.math.log(output / (1 - output))
 
+        target = tf.cast(target, tf.float32)
+        output = tf.cast(output, tf.float32)
+
         return tf.nn.weighted_cross_entropy_with_logits(labels=target,
                                                        logits=output,
                                                         pos_weight=pos_weight)
